5d8c6086060f8402f5b8577c71c185ed4cc7d198
PauloPHPC <paulophpc@gmail.com>
2023-11-23 11:42:43 +0000

    changes to SEQ.sh

--- code/SEQ.sh
+++ code/SEQ.sh
@@ -5,6 +5,7 @@
 
 THREADS="1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40"
 
-for thread in $THREADS: do
+for thread in $THREADS; do
     export OMP_NUM_THREADS=$thread
-    time ./MDseq.exe < inputdata.txt
+    time ./MDseq.exe < inputdata.
+done
