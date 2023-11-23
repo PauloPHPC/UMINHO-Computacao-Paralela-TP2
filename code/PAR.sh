030ffc7d6bf6c10a05fab9f2ba53196374c40f55
PauloPHPC <paulophpc@gmail.com>
2023-11-23 11:35:04 +0000

    for no PAR.sh

--- code/PAR.sh
+++ code/PAR.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#SBATCH --time=2:00
-#SBATCH --cpus-per-task=2
+#SBATCH --time=10:00
+#SBATCH --cpus-per-task=40
 #SBATCH --partition=cpar
-time ./MDpar.exe < inputdata.txt
+
+THREADS="1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40"
+
+for thread in $THREADS: do
+    export OMP_NUM_THREADS=$thread
+    time ./MDpar.exe < inputdata.txt
