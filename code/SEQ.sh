2f0aaceba68bd5c9a543a064cdbdc0d8da5c91ec
PauloPHPC <paulophpc@gmail.com>
2023-11-24 13:10:14 +0000

    Changes to PAR.sh and SEQ.sh

--- code/SEQ.sh
+++ code/SEQ.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --partition=cpar
 
-THREADS=(1 2 4 8 12 16 20 24 28 32 36 40)
+THREADS="1 2 4 8 12 16 20 24 28 32 36 40"
 
 for thread in $THREADS; do
     export OMP_NUM_THREADS=$thread
