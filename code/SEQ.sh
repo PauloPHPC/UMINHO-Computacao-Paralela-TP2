88dd5feb3d1d6912a3a4a9a450b4aa547d151005
PauloPHPC <paulophpc@gmail.com>
2023-11-24 12:53:12 +0000

    changes to seq and par

--- code/SEQ.sh
+++ code/SEQ.sh
@@ -3,9 +3,10 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --partition=cpar
 
-THREADS="1 2 4 8 12 16 20 24 28 32 36 40"
+THREADS=(1 2 4 8 12 16 20 24 28 32 36 40)
 
 for thread in $THREADS; do
     export OMP_NUM_THREADS=$thread
+    echo ${OMP_NUM_THREADS}
     time ./MDseq.exe < inputdata.txt
 done
